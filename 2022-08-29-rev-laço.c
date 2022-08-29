e5cd30611baacefd897f2a9336074f63c11bfdef
cpmart <cpmart@gmail.com>
2022-08-29 10:54:20 -0300

    corrigi inicialização fatorial

--- 2022-08-29-rev-laço.c
+++ 2022-08-29-rev-laço.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int x;
+    int numero, fatorial=1;
+    numero = 5;
+    for(int i=numero; i>=1; i--){
+        fatorial *= i;
+    }
+    printf("Fatorial de %d: %d\n", numero, fatorial);
 }
