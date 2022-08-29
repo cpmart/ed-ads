303ffcd9a4de623bdcf65672654cf4f63868d846
cpmart <cpmart@gmail.com>
2022-08-29 11:10:01 -0300

    com os 3 tipos de laço

--- 2022-08-29-rev-laço.c
+++ 2022-08-29-rev-laço.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int numero, fatorial=1;
-    numero = 5;
-    for(int i=numero; i>=1; i--){
+    int numero, i;
+    unsigned long fatorial=1;
+    numero = 20;
+    for(i=numero; i>1; i--){
         fatorial *= i;
     }
-    printf("Fatorial de %d: %d\n", numero, fatorial);
+    printf("FOR - Fatorial de %d: %lu\n", numero, fatorial);
+    i=numero;
+    fatorial = 1;
+    while(i>1){
+        fatorial *= i--;
+    }
+    printf("WHILE - Fatorial de %d: %lu\n", numero, fatorial);
+    i=numero;
+    fatorial = 1;
+    while(i>1){
+        fatorial *= i--;
+    }
+    printf("DO WHILE - Fatorial de %d: %lu\n", numero, fatorial);
 }
